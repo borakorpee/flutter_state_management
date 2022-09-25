aa38c6c4394374317b6bc78ccfefcb3f53110230
Bora Korpe <borakorpee@gmail.com>
2022-09-25 20:17:33 +0300

    cart screen ui update

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import '../app_thema/app_thema.dart';
 import '../providers/cart.dart' show Cart;
@@ -18,13 +20,43 @@ class CartScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.light,
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
         iconTheme: IconThemeData(color: Colors.black),
         titleTextStyle: TextStyle(
             color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
-        title: Text("Your Cart"),
       ),
-      body: Column(children: <Widget>[
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                left: 31,
+                bottom: 31,
+              ),
+              child: Text(
+                'Your cart',
+                style: GoogleFonts.dmSans(
+                    fontWeight: FontWeight.w700, fontSize: 21),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: cart.items.length,
+                itemBuilder: (ctx, i) => CartItem(
+                  cart.items.values.toList()[i].id,
+                  cart.items.values.toList()[i].price,
+                  cart.items.values.toList()[i].quantity,
+                  cart.items.values.toList()[i].title,
+                  cart.items.keys.toList()[i],
+                  cart.items.values.toList()[i].imgUrl,
+                ),
+              ),
+            ),
+            /*
         Card(
           margin: EdgeInsets.all(15),
           child: Padding(
@@ -57,22 +89,8 @@ class CartScreen extends StatelessWidget {
               ],
             ),
           ),
-        ),
-        SizedBox(height: 10),
-        Expanded(
-          child: ListView.builder(
-            itemCount: cart.items.length,
-            itemBuilder: (ctx, i) => CartItem(
-              cart.items.values.toList()[i].id,
-              cart.items.values.toList()[i].price,
-              cart.items.values.toList()[i].quantity,
-              cart.items.values.toList()[i].title,
-              cart.items.keys.toList()[i],
-              cart.items.values.toList()[i].imgUrl,
-            ),
-          ),
-        ),
-      ]),
+        ),*/
+          ]),
     );
   }
 }
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cart.dart';
@@ -23,7 +24,106 @@ class CartItem extends StatelessWidget {
   );
   @override
   Widget build(BuildContext context) {
-    return Dismissible(
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 31,
+        right: 31,
+        bottom: 22,
+      ),
+      child: Stack(
+        children: <Widget>[
+          SizedBox(
+            width: 365,
+            height: 135,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              elevation: 5,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        EdgeInsets.only(left: 17, bottom: 5, top: 4, right: 10),
+                    child: SizedBox(
+                      width: 100,
+                      height: 95,
+                      child: Image.network(imgUrl),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20, bottom: 19),
+                    child: VerticalDivider(
+                      thickness: 2,
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 15, right: 15, left: 17),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            title,
+                            style: GoogleFonts.dmSans(
+                              color: Color(0xFF000000),
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                            ),
+                          ),
+                          SizedBox(height: 6),
+                          Container(
+                            width: 150,
+                            height: 51,
+                            child: Expanded(
+                              child: Text(
+                                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+                                softWrap: false,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                                style: GoogleFonts.dmSans(
+                                    fontSize: 9, fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            price.toString(),
+                            style: GoogleFonts.dmSans(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              top: 0,
+              right: 0,
+              child: GestureDetector(
+                onTap: () {
+                  Provider.of<Cart>(context, listen: false)
+                      .removeItem(productId);
+                },
+                child: Container(
+                  width: 21,
+                  height: 21,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: Colors.red),
+                  child: Icon(
+                    Icons.remove,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+              )),
+        ],
+      ),
+    ); /*Dismissible(
       key: ValueKey(id),
       background: Container(
         color: Colors.red,
@@ -84,6 +184,6 @@ class CartItem extends StatelessWidget {
           ),
         ),
       ),
-    );
+    );*/
   }
 }
