965ef7ba98130e4ab4f879aece8531b071e026cb
Bora Korpe <borakorpee@gmail.com>
2022-09-25 21:18:56 +0300

    cart screen update

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,12 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_complete_guide/widgets/new_order_item.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import '../app_thema/app_thema.dart';
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart' show Orders;
 import '../widgets/cart_item.dart';
-import '../providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -15,8 +15,8 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final appThema app_thema = appThema();
     final cart = Provider.of<Cart>(context);
+    final orderData = Provider.of<Orders>(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -30,67 +30,113 @@ class CartScreen extends StatelessWidget {
             color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),
       ),
       body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(
-                left: 31,
-                bottom: 31,
-              ),
-              child: Text(
-                'Your cart',
-                style: GoogleFonts.dmSans(
-                    fontWeight: FontWeight.w700, fontSize: 21),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              left: 31,
+              bottom: 31,
+            ),
+            child: Text(
+              'Your cart',
+              style:
+                  GoogleFonts.dmSans(fontWeight: FontWeight.w700, fontSize: 21),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 400,
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => CartItem(
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
+                cart.items.keys.toList()[i],
+                cart.items.values.toList()[i].imgUrl,
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: cart.items.length,
-                itemBuilder: (ctx, i) => CartItem(
-                  cart.items.values.toList()[i].id,
-                  cart.items.values.toList()[i].price,
-                  cart.items.values.toList()[i].quantity,
-                  cart.items.values.toList()[i].title,
-                  cart.items.keys.toList()[i],
-                  cart.items.values.toList()[i].imgUrl,
-                ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 50,
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => NewOrderItem(
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
+                cart.items.keys.toList()[i],
+                cart.items.values.toList()[i].imgUrl,
               ),
             ),
-            /*
-        Card(
-          margin: EdgeInsets.all(15),
-          child: Padding(
-            padding: EdgeInsets.all(8),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 31),
+            child: Divider(
+              thickness: 2,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 31),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Text(
-                  'Total',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Spacer(),
-                Chip(
-                  backgroundColor: app_thema.darkest,
-                  label: Text(
-                    '\$${cart.totalAmount.toStringAsFixed(2)}',
-                    style: TextStyle(color: app_thema.white),
+                  'Total:',
+                  style: GoogleFonts.dmSans(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14,
                   ),
                 ),
-                FlatButton(
-                  onPressed: () {
-                    Provider.of<Orders>(context, listen: false).addOrder(
-                      cart.items.values.toList(),
-                      cart.totalAmount,
-                    );
-                    cart.clear();
-                  },
-                  child: Text('order now'.toUpperCase()),
-                ),
+                Text(
+                  '\$${cart.totalAmount.toStringAsFixed(2)}',
+                  style: GoogleFonts.dmSans(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 14,
+                  ),
+                )
               ],
             ),
-          ),
-        ),*/
-          ]),
+          )
+        ],
+      ),
     );
   }
 }
+   /*
+            Card(
+              margin: EdgeInsets.all(15),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Total',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Spacer(),
+                    Chip(
+                      backgroundColor: app_thema.darkest,
+                      label: Text(
+                        '\$${cart.totalAmount.toStringAsFixed(2)}',
+                        style: TextStyle(color: app_thema.white),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(),
+                          cart.totalAmount,
+                        );
+                        cart.clear();
+                      },
+                      child: Text('order now'.toUpperCase()),
+                    ),
+                  ],
+                ),
+              ),
+            ),*/
---
+++ lib/widgets/new_order_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:math';
+import 'package:intl/intl.dart';
+
+class NewOrderItem extends StatefulWidget {
+  final String id;
+  final double price;
+  final int quantity;
+  final String title;
+  final String productId;
+  final String imgUrl;
+  NewOrderItem(this.id, this.price, this.quantity, this.title, this.productId,
+      this.imgUrl);
+
+  @override
+  State<NewOrderItem> createState() => _NewOrderItemState();
+}
+
+class _NewOrderItemState extends State<NewOrderItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 31),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '${widget.quantity}x ${widget.title}',
+                style: GoogleFonts.dmSans(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14,
+                ),
+              ),
+              Text(
+                widget.price.toString(),
+                style: GoogleFonts.dmSans(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
