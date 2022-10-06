9794739c4bc68a33daaacb2b322748bd1033ab9d
Bora Korpe <borakorpee@gmail.com>
2022-10-06 21:10:05 +0300

    signUp user

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_complete_guide/providers/auth.dart';
 import 'package:flutter_complete_guide/providers/cart.dart';
 import 'package:flutter_complete_guide/screens/cart_screen.dart';
-import 'package:flutter_complete_guide/screens/detail_page/product_detail_screen.dart';
 import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
 import 'package:flutter_complete_guide/screens/user_porducts_screen.dart';
-import './screens/products_overview_screen.dart';
 import 'providers/products_provider.dart';
 import 'package:provider/provider.dart';
 import './providers/orders.dart';
@@ -28,6 +27,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => Orders(),
         ),
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -49,6 +51,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         home: AuthScreen(),
+        //home: AuthScreen(),
         routes: {
           //ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> signUp(String email, String password) async {
+    const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDTcZ5hrovLmEGFzevfDRGCooZ-KLc4lRA';
+    final response = await http.post(
+      Uri.parse(url),
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+}
