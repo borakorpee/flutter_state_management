7dfd4ff730a4f3f0fe55e485ee1a037c23f824a1
Bora Korpe <borakorpee@gmail.com>
2022-10-06 21:30:28 +0300

    api key

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import '../models/apikey.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  static const String googleAPI = GoogleAPI.APIKEY;
 
   Future<void> signUp(String email, String password) async {
     const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDTcZ5hrovLmEGFzevfDRGCooZ-KLc4lRA';
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${googleAPI}';
     final response = await http.post(
       Uri.parse(url),
       body: json.encode(
